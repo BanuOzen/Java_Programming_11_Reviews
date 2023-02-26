3818286da03d5a26305971bf849b211cad4ad2a7
Banu <banu2106@gmail.com>
2023-02-26 23:05:32 +0300

    Initial commit

---
+++ src/week_01/HelloCydeo.java
@@ -0,0 +1,11 @@
+package week_01;
+
+public class HelloCydeo {
+    // in order to create main method type "main" hit the enter
+    public static void main (String[] args){
+
+        // in order to show the println statement you can type "sout" and hit the enter
+        System.out.println("Hello Cydeo!");
+
+    }
+}
