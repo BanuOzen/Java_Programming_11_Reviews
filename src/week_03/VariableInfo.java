910e63e6dddd76dee5a1da33a8e35d21a76d04ca
Banu <banu2106@gmail.com>
2023-03-05 17:46:04 +0300

    week3

---
+++ src/week_03/VariableInfo.java
@@ -0,0 +1,52 @@
+package week_03;
+
+public class VariableInfo {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double num = 10;
+
+        float floatVar = 12.5f;
+        float floatVar2 = (float)14.5;//explicit casting
+
+        long l = 111_111_111_110L;
+
+        int i = 12;
+        short s = 2;
+        byte b = 127; // -128  127
+
+        char ch = '%';
+        char ch1 = 'd';
+        char ch2 = '2';
+
+
+        boolean bool = true;
+        boolean boo1l = false;
+
+        String str = "Adam";
+
+        //========================
+        // implicit vs explicit
+
+        double dd=10;
+        long ll =10;
+        int ii =10;
+        short ss =10;
+
+        dd = ss;
+        ll = ii;
+
+        // explicit casting
+        ii = (int)dd; // if we want to put bigger to smaller we have to do aht action as a developer
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
