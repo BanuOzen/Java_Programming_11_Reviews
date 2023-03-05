07ae4dbfa913d7be422df6a1b142d88b154038ba
Banu <banu2106@gmail.com>
2023-03-05 18:28:56 +0300

    week3

---
+++ src/week_03/VariableInfo2.java
@@ -0,0 +1,55 @@
+package week_03;
+
+public class VariableInfo2 {
+    public static void main(String[] args) {
+
+
+    /*
+
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+        System.out.println("Banu");
+
+     */
+        String name = "Banu";
+
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+
+        //addition
+        System.out.println(23+4);
+        System.out.println('c'+ 4);
+
+
+        System.out.println(""+12+4);
+
+        String a = "" + 12; // it is not number
+
+        String result = a+4;//it is not sixteen it is onetwofour
+
+                         //175
+                        // 17 (String) +5
+                        // 17 + "" + 5
+        System.out.println(12+5+""+5);
+
+        System.out.println(12 + "" +(5+4)+ 1);
+
+
+
+
+
+
+
+    }
+}
