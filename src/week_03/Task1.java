749d5f9b26497ea45e402b34c6e6abc2fe7c26a7
Banu <banu2106@gmail.com>
2023-03-05 16:50:29 +0300

    eu11

--- src/week_03/Task1.java
+++ src/week_03/Task1.java
@@ -2,6 +2,23 @@ package week_03;
 
 public class Task1 {
     public static void main(String[] args) {
+        System.out.print("Adam\n");
+        System.out.print("Eyup");
+
+        System.out.println();
+
+
+        // \t paragraph space(tab)
+        System.out.print("Adam\tEyup\n");
+
+        //\\ single bf slash
+        System.out.println("Computer\\laptop");
+
+        // Car("Mercedes")
+        System.out.println("Car(\"Mercedes\")");
+
+        System.out.print("*\n* *\n* * * \n* * * *\n* * * * *");
+
 
 
     }
