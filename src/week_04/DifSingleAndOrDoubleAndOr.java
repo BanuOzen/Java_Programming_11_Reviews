686e335065156aca3f5487cd110d1c08f9bce9c8
Banu <banu2106@gmail.com>
2023-03-12 17:17:28 +0300

    week_04

---
+++ src/week_04/DifSingleAndOrDoubleAndOr.java
@@ -0,0 +1,33 @@
+package week_04;
+
+public class DifSingleAndOrDoubleAndOr {
+    public static void main(String[] args) {
+
+        int a=10;
+        boolean value=(4<5)&(a++==10);
+
+        System.out.println("value = " + value);
+
+        System.out.println(a);
+
+
+        /*
+        if you are using &&( double and &&) operators
+        if the first one is F we will not look at
+        the second one
+         */
+        int b=10;     //   T |  T
+        boolean valueOr=(4>3)||(--b==9);
+        System.out.println("valueOr = " + valueOr);
+        System.out.println("b = " + b);
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/week_04/LogicalOperators.java
@@ -0,0 +1,21 @@
+package week_04;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+
+        System.out.println((5<6)&(6==6) );//true
+        System.out.println((5<6)&(6!=6));//False
+
+        System.out.println((3==5)|(3<=10));
+        boolean value=5<9;
+
+        System.out.println("value = " + value); //true
+
+        System.out.println("!value = " + !value); //false
+
+
+
+
+    }
+}
