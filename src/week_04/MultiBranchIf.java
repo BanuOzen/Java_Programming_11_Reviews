c3ff0721fa79d1dd1da5c9cc5955159c97d97bfd
Banu <banu2106@gmail.com>
2023-03-12 17:59:48 +0300

    week_04

---
+++ src/week_04/MultiBranchIf.java
@@ -0,0 +1,21 @@
+package week_04;
+
+public class MultiBranchIf {
+    public static void main(String[] args) {
+        int number=80;
+
+        if(number>0){
+            System.out.println(number + " is positive");
+        }else if(number<0){
+            System.out.println(number + " is negative");
+        }else{
+            System.out.println(number + " is zero");
+        }
+
+
+
+
+
+
+    }
+}
