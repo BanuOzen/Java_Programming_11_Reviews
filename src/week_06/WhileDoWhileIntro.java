4e9f0ed1c85c3721b333ebf71cb87ce9fbeb55fc
Banu <banu2106@gmail.com>
2023-03-26 15:22:59 +0300

    week_06

--- src/week_06/WhileDoWhileIntro.java
+++ src/week_06/WhileDoWhileIntro.java
@@ -1,19 +1,22 @@
 package week_06;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class WhileDoWhileIntro {
     public static void main(String[] args) {
-        System.out.println("Do you want to play game?");
+
+       // System.out.println("Do you want to play game?");
+
         Scanner scan =new Scanner(System.in);
-        String ans=scan.next();//yes or no
+        //String ans=scan.next();//yes or no
        /*
         if(ans.equals("Yes")){
 
         }
 
         */
-        int hidden=6;
+       /* int hidden=6;
         while(ans.equals("Yes")){
 
             System.out.println("Try to know the number");
@@ -27,10 +30,24 @@ public class WhileDoWhileIntro {
             System.out.println("Do you want to go on playing");
             ans=scan.next();
 
+        */
+        Random random =new Random();
+        int hiddenNumber = random.nextInt(25);
+        boolean flag=true;
 
+        System.out.println("Game is starting");
+        System.out.println("Try to find the number");
 
+        do{
+            int num=scan.nextInt();
+            if(num==hiddenNumber){
+                flag=false;
+                System.out.println("Found the number");
+            }else{
+                System.out.println("Try to another number");
+            }
 
-        }
+        } while(flag);
 
 
     }
