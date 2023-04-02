65d4ca91d877966efa9ed145a1cbefb77dcb4c4c
Banu <banu2106@gmail.com>
2023-04-02 16:58:18 +0300

    week_07

---
+++ src/week_07/Task4.java
@@ -0,0 +1,46 @@
+package week_07;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String[] array = {"java", "longer word", "civic", "apple", "racecar", "mom", "anna"};
+
+       String longest ="";
+        for (String s : array){
+            // WE need to check the word is palindrome or not
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < s.length()/2; i++) {
+                char letterFromBeginning = s.charAt(i);   // 0 1
+                char letterFromLast = s.charAt(s.length()-1-i); // 3 2
+                if(letterFromBeginning!=letterFromLast){
+                    isPalindrome = false;
+                    break;
+                }
+
+            }
+            // we need to find the longest palindrome
+            if(isPalindrome && s.length()>longest.length()){
+                longest = s;
+            }
+
+        }
+        String result = longest.isEmpty() ? "There is no palindrome" : longest;
+        System.out.println(result);
+
+
+
+    }
+}
+/*
+Task 4 :  For Each - Nested For
+
+            Longest Palindrome
+            Given a String array.
+             Find the longest Palindrome String in your array.
+Ex:
+Input: ["java", "longer word", "civic" "apple", "racecar", "mom", "anna"]
+Output: racecar
+Ex:
+Input: ["abc", "dna", "kevin", "joe", "lamp"]
+Output: No palindrome
+ */
