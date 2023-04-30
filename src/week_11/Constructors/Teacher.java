83140fe535d319e03427d8d770b486bf75f9ea1a
Banu <banu2106@gmail.com>
2023-04-30 17:51:58 +0300

    week_11

---
+++ src/week_11/Constructors/Teacher.java
@@ -0,0 +1,29 @@
+package week_11.Constructors;
+
+public class Teacher {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public Teacher(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Teacher(){
+
+    }
+
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
