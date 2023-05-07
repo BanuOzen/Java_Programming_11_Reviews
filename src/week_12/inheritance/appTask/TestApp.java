040d604b267ecc59fcc3249fa0b6879ccbbc7993
Banu <banu2106@gmail.com>
2023-05-07 17:44:45 +0300

    week_12

---
+++ src/week_12/inheritance/appTask/TestApp.java
@@ -0,0 +1,16 @@
+package week_12.inheritance.appTask;
+
+public class TestApp {
+    public static void main(String[] args) {
+
+        Instagram instagram = new Instagram(2.3);
+        instagram.download();
+        System.out.println(instagram);
+
+        Discord discord = new Discord(1.3);
+        discord.chat("Ahmet");
+
+
+
+    }
+}
